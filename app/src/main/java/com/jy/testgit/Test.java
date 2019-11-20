3e895a6550a89683bbb7326482f1d8f158c605da
taofu <taofujob@gmail.com>
2019-11-20 15:17:13 +0800

    lisi 增加 Test class

---
+++ app/src/main/java/com/jy/testgit/Test.java
@@ -0,0 +1,12 @@
+package com.jy.testgit;
+
+/*
+ * created by Cherry on 2019-11-20
+ **/
+public class Test {
+
+
+    public static void main(String args []){
+        System.out.println("lisi print msg");
+    }
+}
